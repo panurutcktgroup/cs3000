0d6faa2232f082a6f4c8209cb68af53a4ad75bc2
gracenxt <panurut.n@cktgroup.tech>
2026-08-21 08:46:13 +0700

    1

--- Core/App.cs
+++ Core/App.cs
@@ -1,4 +1,4 @@
-﻿using CompanyCLI.UI;
+using CompanyCLI.UI;
 using Spectre.Console;
 
 namespace CompanyCLI.Core;
@@ -10,6 +10,7 @@ public class App
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.InputEncoding = System.Text.Encoding.UTF8;
 
+        await StartupUI.ShowAsync();
         await DashboardUI.ShowAsync();
     }
 }
